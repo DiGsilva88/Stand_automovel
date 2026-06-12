7f3a72b6e967f17dab51d51054ece79425c1651b
DianaS <diana.silva.257306188@msft.cesae.pt>
2026-06-12 16:27:29 +0100

    Rotas alteradas

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,16 @@ use App\Http\Controllers\DashboardController;
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->name('dashboard');
 
+Route::get('/viaturas', [ViaturaController::class, 'index']);
+Route::get('/viaturas/{viatura}', [ViaturaController::class, 'show']);
+
 Route::middleware(['auth'])->group(function () {
 
-    Route::resource('viaturas', ViaturaController::class);
     Route::resource('clientes', ClienteController::class);
+
     Route::resource('vendas', VendaController::class);
+
+    Route::resource('viaturas', ViaturaController::class)
+         ->except(['index', 'show']);
 
 });
